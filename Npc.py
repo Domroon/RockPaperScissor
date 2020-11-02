c64a7af5126c3cd4311f5266824a79fdc943c237
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-02 18:58:33 +0100

    Add get_choice method in Npc class

--- Npc.py
+++ Npc.py
@@ -1,9 +1,19 @@
+import random
+
 class Npc:
     def __init__(self):
         self.score = 0
+        self.choice = [
+            "rock",
+            "paper",
+            "scissor"
+        ]
 
     def get_score(self):
         return self.score
 
     def set_score(self, score):
         self.score = score
+
+    def get_choice(self):
+        return self.choice[random.randint(0, 2)]
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from User import User
 from Npc import Npc
+# import random
 
 print("Welcome to the Game Rock, Paper or Scissor!\n")
 
@@ -8,7 +9,8 @@ computer = Npc()
 
 print(f"Hello {user.get_name()}. Lets start the Game!")
 
-#while(user.get_score() < 3 and computer.get_score() < 3):
+# while(user.get_score() < 3 and computer.get_score() < 3):
 
 user.set_choice("mist")
 print(user.get_choice())
+print(computer.get_choice())
