d4fb4de0b80c182449f7b142af2287785f41b2e3
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-02 21:33:31 +0100

    Some test stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from User import User
 from Npc import Npc
 # import random
+import time
 
 print("Welcome to the Game Rock, Paper or Scissor!\n")
 
@@ -14,3 +15,18 @@ print(f"Hello {user.get_name()}. Lets start the Game!")
 user.set_choice("mist")
 print(user.get_choice())
 print(computer.get_choice())
+
+time.sleep(1)
+print("Rock")
+time.sleep(2)
+print("Paper")
+time.sleep(2)
+print("Scissor")
+time.sleep(2)
+
+# Willkommensnachricht
+# Eingabe des Namens
+# Anzeige Schere Stein Papier
+# Anzeige verloren oder gewonnen
+# Ausgabe von Punktestand NPC - Spieler
+# Wiederholen wenn einer noch keine 3 Punkte hat
